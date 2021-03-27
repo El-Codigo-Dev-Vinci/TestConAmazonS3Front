2ea493f192ca5a858b88decfc0cc799af717440e
joaquinpettinari <joaquinpettinari@hotmail.com>
2021-03-27 01:59:32 -0300

    Adding recordrtc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import DatosUsuario from './components/DatosUsuario';
 import Home from './components/Home';
 
 const useStyles = makeStyles(() => ({
@@ -14,12 +13,9 @@ export default function App() {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="sm" className={classes.root}>
+    <Container>
       <Router>
         <Switch>
-          <Route path="/usuarios/:id">
-            <DatosUsuario />
-          </Route>
           <Route path="/">
             <Home />
           </Route>
